2fc51c2b3395bc357a084bc6028244be896a48b0
isvar <40363202+isvar@users.noreply.github.com>
2021-02-28 18:11:23 -0600

    Update graph_cup.m
    Add labels and corrected name of variables

--- graph_cup.m
+++ graph_cup.m
@@ -21,8 +21,11 @@ y_4 = -x_4/2.5+5.4;
 plot(x_4,y_4)
 
 x_alt = 2:0.1:9;
-y_alt = -0.012.*x.^2 + 0.39.*x - 0.7;
+y_alt = -0.012.*x_alt.^2 + 0.39.*x_alt - 0.7;
 plot(x_alt,y_alt)
+title("Grafica por Secciones");
+xlabel("X");
+ylabel("Y");
 
 
 nexttile;
@@ -30,6 +33,9 @@ xand = [x_1 x_2 x_3 x_4];
 yand = [y_1 y_2 y_3 y_4];
 plot(xand, yand)
 hold on;
+title("Grafica Completa");
+xlabel("X");
+ylabel("Y");
 
 nexttile;
 y = [0 y_2 y_4];
@@ -38,4 +44,8 @@ y_alt2 = [0 y_alt];
 cylinder(y)
 hold on;
 cylinder(y_alt2)
+title("Modelo 3D del Vaso");
+xlabel("X");
+ylabel("Y");
+zlabel("Z");
 
